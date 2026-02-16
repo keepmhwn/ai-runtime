aaee9dcb31e90586cf041aa9146acfd3f0e58c6c
kimmeh <mhko0411@gmail.com>
2026-02-16 15:53:32 +0900

    feat: vision 좌표 변환 유틸리티 export 추가
    - useImageDimensionScale 훅 및 관련 타입 export
    - CoordinateTransformUtils 및 변환 함수들 export

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,1 +1,18 @@
 export { useTextStream, type UseTextStreamOptions } from "./useTextStream";
+export {
+  useImageDimensionScale,
+  type UseImageDimensionScaleConfig,
+  type UseImageDimensionScaleReturn,
+  type Point,
+  type Polygon,
+  type BoundingBox,
+  type ScaleRatio,
+  type Dimensions,
+  CoordinateTransformUtils,
+  isPoint,
+  isPolygon,
+  isBoundingBox,
+  transformPoint,
+  transformPolygon,
+  transformBoundingBox,
+} from "./vision/useImageDimensionScale";
